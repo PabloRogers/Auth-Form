75045242d9a966e3ecf7202d77b2d41bde3abddc
Pablo Rogers <realpablorogers@gmail.com>
2024-08-04 16:31:50 -0500

    fixed formatting

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default async function Home() {
     <div>
       <div>Hello {user?.primaryEmailAddress?.emailAddress}</div>
       <div>
-        
         <SignOutButton />
       </div>
     </div>
