48753d4f40e0b0ccde6f996ff44a1455ee5559c4
Pablo Rogers <realpablorogers@gmail.com>
2024-08-04 14:01:56 -0500

    removed /form route

--- app/form/page.tsx
+++
@@ -1,7 +0,0 @@
-import FormDemo from "@/components/FormDemo";
-import ForgotPasswordReset from "@/components/auth/ForgotPasswordStep3";
-import React from "react";
-
-export default function page() {
-  return <div />;
-}
