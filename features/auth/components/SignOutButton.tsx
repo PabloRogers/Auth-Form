96d19c08697b86191b6d228aefa8c2f40dd7e203
Pablo Rogers <realpablorogers@gmail.com>
2024-10-11 10:23:32 -0400

    moved SignOutButton to correct file

--- features/auth/components/SignOutButton.tsx
+++ features/auth/components/SignOutButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useClerk } from "@clerk/nextjs";
-import { Button } from "./ui/button";
+import { Button } from "../../../components/ui/button";
 import { toast } from "sonner";
 
 export default function SignOutButton({}) {
